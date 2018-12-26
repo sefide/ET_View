3f5bf048c8ec35992f3711ed28de9d6c51bfccdc
박현우 <@DESKTOP-27SP5R2>
2018-12-27 00:00:36 +0900

    관리자페이지에서 제휴사 추가하는 기능 70%

--- src/com/kh/et/company/model/dao/CompanyDao.java
+++ src/com/kh/et/company/model/dao/CompanyDao.java
@@ -1,5 +1,57 @@
 package com.kh.et.company.model.dao;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import com.kh.et.company.model.vo.Company;
+import static com.kh.et.common.JDBCTemplate.*;
+
+
 public class CompanyDao {
+	
+	private Properties prop=new Properties();
+	
+	public CompanyDao() {
+		
+	String fileName=CompanyDao.class.getResource("/sql/company/company-query.properties").getPath();
+	try {
+		prop.load(new FileReader(fileName));
+	} catch (IOException e) {
+		
+		e.printStackTrace();
+	}
+	
+	
+	
+	}
+	public int insertCompany(Connection con, Company reqCompany) {
+		PreparedStatement pstmt=null;
+		int result=0;
+		String query=prop.getProperty("insertCompany");
+		
+		try {
+			pstmt=con.prepareStatement(query);
+			pstmt.setString(1, reqCompany.getC_name());
+			pstmt.setString(2, reqCompany.getC_biss_num());
+			pstmt.setString(3, reqCompany.getC_represent());
+			pstmt.setDate(4, reqCompany.getC_date());
+			pstmt.setDate(5, reqCompany.getC_end_date());
+			pstmt.setString(6, reqCompany.getC_phone());
+			
+			result=pstmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			close(pstmt);
+		}
+		
+		return result;
+	}
 
 }
--- src/com/kh/et/company/model/service/CompanyService.java
+++ src/com/kh/et/company/model/service/CompanyService.java
@@ -1,5 +1,29 @@
 package com.kh.et.company.model.service;
 
+import static com.kh.et.common.JDBCTemplate.commit;
+import static com.kh.et.common.JDBCTemplate.getConnection;
+import static com.kh.et.common.JDBCTemplate.rollback;
+
+import java.sql.Connection;
+
+import com.kh.et.company.model.dao.CompanyDao;
+import com.kh.et.company.model.vo.Company;
+import com.kh.et.member.model.dao.MemberDao;
+
 public class CompanyService {
+
+	public int insertCompany(Company reqCompany) {
+Connection con=getConnection();
+		
+		int result= new CompanyDao().insertCompany(con,reqCompany);
+		
+		if(result>0) {
+			commit(con);
+			
+		}else {
+			rollback(con);
+		}
+		return result;
+	}
 
 }
