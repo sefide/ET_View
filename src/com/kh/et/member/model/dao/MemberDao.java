629df6360a1ca5b141c52b70504f342868085683
dofls <dofls@DESKTOP-OQQLU40>
2018-12-26 18:03:04 +0900

    일반회원 로그인, 회원가입 수정

--- src/com/kh/et/member/model/dao/MemberDao.java
+++ src/com/kh/et/member/model/dao/MemberDao.java
@@ -2,6 +2,7 @@ package com.kh.et.member.model.dao;
 
 import static com.kh.et.common.JDBCTemplate.close;
 
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -11,7 +12,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 
-import com.kh.et.member.model.vo.Company;
+
 import com.kh.et.member.model.vo.Member;
 
 public class MemberDao {
@@ -73,6 +74,36 @@ public class MemberDao {
 		
 		
 		return loginUser;
+	}
+
+	public int insertMember(Connection con, Member reqMember) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		
+		String query = prop.getProperty("insertMember");
+		try {
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, reqMember.getM_id());
+			pstmt.setString(2, reqMember.getM_pwd());
+			pstmt.setString(3, reqMember.getM_name());
+			pstmt.setString(4, reqMember.getM_email());
+			pstmt.setString(5, "테스트입니다");
+			pstmt.setInt(6, 200);
+			pstmt.setInt(7, 3);
+			pstmt.setString(8, reqMember.getM_question());
+			pstmt.setString(9, reqMember.getM_answer());
+			
+			result = pstmt.executeUpdate();
+			
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(pstmt);
+		}
+		
+		return result;
 	}
 
 	/*//일반회원 회원가입용 메소드
@@ -188,47 +219,7 @@ public class MemberDao {
 		return loginManager;
 	}*/
 
-	//제휴사 로그인 메소드
-	public Company companyLogin(Connection con, Company reqMember) {
-		PreparedStatement pstmt = null;
-		ResultSet rset = null;
-		Company loginCompany = null;
-		
-		String query = prop.getProperty("companyLogin");
-		try {
-			pstmt = con.prepareStatement(query);
-			pstmt.setString(1, reqMember.getC_id());
-			pstmt.setString(2, reqMember.getC_pwd());
-			
-			rset = pstmt.executeQuery();
-			
-			if(rset.next()) {
-				loginCompany = new Company();
-				
-				loginCompany.setC_no(rset.getInt("C_NO"));
-				loginCompany.setC_name(rset.getString("C_NAME"));
-				loginCompany.setC_biss_num(rset.getString("C_BISS_NUM"));
-				loginCompany.setC_id(rset.getString("C_ID"));
-				loginCompany.setC_pwd(rset.getString("C_PWD"));
-				loginCompany.setC_phone(rset.getString("C_PHONE"));
-				loginCompany.setC_email(rset.getString("C_EMAIL"));
-				loginCompany.setC_category(rset.getString("C_CATEGORY"));
-				loginCompany.setC_biss_address(rset.getString("C_BISS_ADDRESS"));
-				loginCompany.setC_date(rset.getDate("C_DATE"));
-				loginCompany.setC_end_date(rset.getDate("C_END_DATE"));
-				loginCompany.setC_status(rset.getString("C_STATUS"));
-			}
-			
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}finally {
-			close(con);
-			close(rset);
-		}
-		
-		return loginCompany;
-	}
+	
 
 	/*public int updateMember(Connection con, MemberNormal reqMember) {
 		// TODO Auto-generated method stub
