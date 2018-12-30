095c8f230a87e6e6095c39d1c64d0eb2aca93f83
dofls <dofls@DESKTOP-OQQLU40>
2018-12-31 02:46:06 +0900

    회원가입 이메일 인증, 아이디 찾기, 임시비밀번호 찾기 하는중....하..

--- src/com/kh/et/member/controller/Member_IdSearch_Servlet.java
+++ src/com/kh/et/member/controller/Member_IdSearch_Servlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
 import com.kh.et.member.model.service.MemberService;
 import com.kh.et.member.model.vo.Member;
 
@@ -48,16 +49,24 @@ public class Member_IdSearch_Servlet extends HttpServlet {
 		
 		Member loginUser = new MemberService().memberIdSearch(reqMember);
 		
+		
+		String userId = loginUser.getM_id();	//변수 userId에 loginUer에 있는 아이디를 가져와서 저장
+		
+		System.out.println("질문 : " + userId);
+		System.out.println("질문 : " + loginUser.getM_question());
+		System.out.println("답 : " + loginUser.getM_answer());
+		System.out.println("아이디: " + loginUser.getM_id());
+		
 		PrintWriter out = response.getWriter();
 		
-		if(loginUser != null) {
-			String userId = loginUser.getM_id();	//변수 userId에 loginUer에 있는 아이디를 가져와서 저장
-			out.append("success");
+		if(userId != null) {
+			out.append("({'result':'입력하신 정보와 일치하는 아이디는 ' + userId + '입니다'})");
 		}else {
 			out.append("fail");
 		}
+		
+		
 	}
-
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
